7580957f7703e3cf2c5d401d68a8e5ed883a4bf4
Xiaofeng Zhang(HUST) <zhangxiaofengultrasound@gmail.com>
2026-04-27 12:43:42 +0800

    config/

---
+++ config_ssl.py
@@ -0,0 +1,12 @@
+# config_ssl.py
+
+SSL_CONFIG = {
+    "epochs": 500,
+    "batch_size": 12,
+    "lr": 1e-4,
+    "weight_decay": 1e-4,
+    "temperature": 0.5,
+    "proj_dim": 128,
+    "seed": 2026,
+    "save_freq": 50
+}
